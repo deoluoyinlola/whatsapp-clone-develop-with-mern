8c4788b96eb6f71a370632e05b1e4caa056b34c8
Adeolu Oyinlola <adeoluoyin@gmail.com>
2022-03-07 11:25:33 -0800

    set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styled from "styled-components";
+import ContactListComponent from "./components/ContactListComponent";
+import ConversationComponent from "./components/ConversationComponent";
 
-function App() {
+const Container = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  background: #f8f9fb;
+`;
+
+const ChatPlaceholder = styled.img`
+  width: 240px;
+  height: 240px;
+  border-radius: 50%;
+  object-fit: contain;
+`;
+const Placeholder = styled.div`
+  flex: 3;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 14px;
+  gap: 10px;
+  color: rgba(0, 0, 0, 0.45);
+
+  span {
+    font-size: 32px;
+    color: #525252;
+  }
+`;
+
+function App(props) {
+  const { userInfo } = props;
+  const [selectedChat, setChat] = useState();
+  const [refreshContactList, toggleRefreshContactList] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <ContactListComponent
+        setChat={setChat}
+        userInfo={userInfo}
+        refreshContactList={refreshContactList}
+      />
+      {selectedChat ? (
+        <ConversationComponent
+          selectedChat={selectedChat}
+          userInfo={userInfo}
+          refreshContactList={() =>
+            toggleRefreshContactList(!refreshContactList)
+          }
+        />
+      ) : (
+        <Placeholder>
+          <ChatPlaceholder src="/whatsapp-clone/welcome-placeholder.jpeg" />
+          <span>Keep your phone connected</span>
+          WhatsApp connects to your phone to sync messages.
+        </Placeholder>
+      )}
+    </Container>
   );
 }
 
